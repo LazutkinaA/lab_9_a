ae6d396daa71f3f674d1529677d99acf05937977
Anastasia Lazutkina <lazutkinaanastasia3@gmail.com>
2020-07-02 21:38:57 +0300

    output was added

--- lab9_15.cpp
+++ lab9_15.cpp
@@ -117,3 +117,55 @@ int mas_y(int y, int x[], int& m, int i)		// Рекурсивная функци
 	}
 
 }
+
+void mas_x_y(int a[n][n], int y, int x[])		// Функция вывода выполненной работы программы
+{
+	int i, j, l;
+	cout << "Ваш массив: \n";
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << " ";
+			if (j % 5 == 4)
+			{
+				cout << "\n";
+			}
+		}
+	}
+	cout << "Массив x: \n";
+	for (l = 0; l < n; l++)
+	{
+		cout << x[l] << " ";
+	}
+	cout << "\n";
+	cout << "y=" << y;
+	ofstream f("rezalt.txt");
+	if (!f.is_open())
+	{
+		cout << "Ошибка открытия файла!" << endl;
+	}
+	else
+	{
+		f << "Ваш массив: \n";
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				f << a[i][j] << " ";
+				if (j % 5 == 4)
+				{
+					f << "\n";
+				}
+			}
+		}
+		f << "Массив x: \n";
+		for (l = 0; l < n; l++)
+		{
+			f << x[l] << " ";
+		}
+		f << "\n";
+		f << "y=" << y;
+	}
+	f.close();
+}
